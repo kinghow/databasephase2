66f5dc5e7d4dc3438326ac9f7dd80af74a9185c8
kinghow <kinghoe_w@hotmail.com>
2017-03-26 02:21:36 -0600

    merging

--- src/cs5530/ShowTables.java
+++ src/cs5530/ShowTables.java
@@ -10,11 +10,11 @@ public class ShowTables {
 			ResultSet results = stmt.executeQuery(query);
 			if (results.isBeforeFirst()) {
 				System.out.println("\nYour listings:\n");
-				System.out.printf("| %1$-3s | %2$-15s | %3$-15s | %4$-30s | %5$-10s | %6$-10s | %7$-9s | %8$-25s | %9$-15s | %10$-15s | %11$-10s |\n",
-						"hid", "category", "name", "url", "phone_num", "year_built", "price", "street", "city", "state", "zipcode");
+				System.out.printf("| %1$-3s | %2$-15s | %3$-25s | %4$-30s | %5$-10s | %6$-10s | %7$-11s | %8$-25s | %9$-15s | %10$-15s | %11$-10s |\n",
+						"hid", "category", "name", "url", "phone_num", "year_built", "price/night", "street", "city", "state", "zipcode");
 				
 				while (results.next()) {
-					System.out.printf("| %1$-3d | %2$-15s | %3$-15s | %4$-30s | %5$-10s | %6$-10s | %7$-9.2f | %8$-25s | %9$-15s | %10$-15s | %11$-10s |\n",
+					System.out.printf("| %1$-3d | %2$-15s | %3$-25s | %4$-30s | %5$-10s | %6$-10s | %7$-11.2f | %8$-25s | %9$-15s | %10$-15s | %11$-10s |\n",
 							results.getInt("hid"), results.getString("category"), results.getString("name"), results.getString("url"),
 							results.getString("phone_num"), results.getInt("year_built"), results.getDouble("price"),
 							results.getString("street"), results.getString("city"), results.getString("state"), results.getString("zipcode"));
@@ -24,6 +24,26 @@ public class ShowTables {
 			} else
 				System.out.println("You have no listings.\n");
 		} catch (Exception e) { throw(e); }
-		
+	}
+	
+	public static void displayAvailableUserListings(String login, Statement stmt) throws Exception {
+		try {
+			String query = "SELECT * FROM TH th, Available a, Period p WHERE th.login='"+login+"' AND a.hid=th.hid AND a.pid=p.pid";
+			ResultSet results = stmt.executeQuery(query);
+			if (results.isBeforeFirst()) {
+				System.out.println("\nYour available listings:\n");
+				System.out.printf("| %1$-3s | %2$-25s | %3$-10s | %4$-10s | %5$-11s |\n",
+						"hid", "name", "avail_from", "avail_to", "price/night");
+				
+				while (results.next()) {
+					System.out.printf("| %1$-3d | %2$-25s | %3$-10s | %4$-10s | %5$-11s |\n",
+							results.getInt("th.hid"), results.getString("th.name"), results.getDate("p.fromDate"), results.getString("p.toDate"),
+							results.getString("a.price_per_night"));
+				}
+				
+				System.out.println();
+			} else
+				System.out.println("You have no available listings.\n");
+		} catch (Exception e) { throw(e); }
 	}
 }
---
+++ src/cs5530/UpdateListingDates.java
@@ -0,0 +1,22 @@
+package cs5530;
+
+import java.sql.Statement;
+
+public class UpdateListingDates extends InputSystem {
+
+	public UpdateListingDates(String login) {
+		super(num_of_inputs);
+	}
+
+	@Override
+	public void showInputMessage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void storeInput(String input, Statement stmt) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+}
